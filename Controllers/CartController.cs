55fa3c937258c1e5b8f724cc088cf91e81eaedf3
Yuliia Oleksiienko <mykhaiilooleksiienko@MacBook-Pro-Mykhaiilo.local>
2026-01-16 11:45:27 +0200

    Add CartController to handle pizza quantity validation and cart updates

---
+++ Controllers/CartController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PizzaGo.Services.Interfaces;
+using PizzaGo.DTOs.Requests;
+using System.Linq;
+
+namespace PizzaGo.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] AddToCartRequest request)
+        {
+            if (request.Quantity <= 0)
+            {
+                return BadRequest("Quantity must be more than 0");
+            }
+
+            await _cartService.AddToCartAsync(request.PizzaId, request.Quantity);
+            
+            var items = await _cartService.GetCartItemsAsync();
+            var totalCount = items.Sum(x => x.Quantity);
+            return Ok(totalCount);
+        }   
+    }
+}
