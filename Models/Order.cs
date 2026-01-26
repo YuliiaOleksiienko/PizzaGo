2fe06dae2025f8ee5c0a5cf46368437af2c2daeb
Yuliia Oleksiienko <mykhaiilooleksiienko@MacBook-Pro-Mykhaiilo.local>
2026-01-26 13:27:16 +0200

    Add UserId to models/order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,8 +6,10 @@ namespace PizzaGo.Models
     public class Order
     {
         public int Id { get; set; }
-        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
         public string? UserId { get; set; } = null!;
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        
         public List<OrderDetail> OrderDetails { get; set; } = new();
         public decimal TotalPrice { get; set; }
 
